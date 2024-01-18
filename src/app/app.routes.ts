27f572cfbb39c5bf28112d2dd4a2f9f6b746c068
tomeksen <tomas.olmeda@hotmail.com>
2024-01-17 18:46:38 -0800

    adding routing to the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,52 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AdminLoginComponent } from './admin/admin-login/admin-login.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { UserCrudComponent } from './admin/user-crud/user-crud.component';
+import { ProductComponent } from './product/product.component';
+import { SigninSignupComponent } from './customer/signin-signup/signin-signup.component';
+import { SellerDashboardComponent } from './customer/seller/seller-dashboard/seller-dashboard.component';
+import { BuyerDashboardComponent } from './customer/buyer/buyer-dashboard/buyer-dashboard.component';
+import { CheckoutComponent } from './customer/buyer/checkout/checkout.component';
+import { PageNotFoundComponent } from './shared/layouts/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {path: "", redirectTo: "/", pathMatch: "full"},
+    {path: "", component : HomeComponent},
+
+    //admin
+    {path: "", children : [
+        {path: "admin-login",component: AdminLoginComponent}
+    ]},
+    {path: "", children : [
+        {path: "admin-dashboard",component: AdminDashboardComponent},
+        {path: "admin/user",component: UserCrudComponent},
+        {path: "admin/product",component: ProductComponent}
+    ]},
+
+    //profile
+    {path: "my-profile", component : UserProfileComponent},
+    {path: "contact-us", component : ContactUsComponent},
+
+    {path: "", children : [
+        {path: "sign-in",component: SigninSignupComponent},
+        {path: "sign-up",component: SigninSignupComponent},
+    ]},
+
+    //Seller
+    {path: "", children : [
+        {path: "seller-dashboard",component: SellerDashboardComponent},
+        {path: "seller/product",component: ProductComponent},
+    ]},
+
+    //buyer
+    {path: "", children : [
+        {path: "buyer-dashboard",component: BuyerDashboardComponent},
+        {path: "checkout",component: CheckoutComponent},
+    ]},
+
+    {path: "**", component : PageNotFoundComponent},
+];
