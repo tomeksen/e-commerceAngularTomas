65f8171734916c8c1652c0d7ce4dc681f4104aa7
tomeksen <tomas.olmeda@hotmail.com>
2024-01-18 16:34:20 -0800

    ading apiService with login and register

--- src/app/shared/services/login-signup.service.ts
+++ src/app/shared/services/login-signup.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginSignupService {
 
-  constructor() { }
+  public login_url = "http://localhost:3000"
+  public reg_url = "http://localhost:3000"
+  
+  constructor(private http: HttpClient,private apiService: ApiService) { }
+
+  authLogin(userName: string, password: string): Observable<any>{
+    return this.apiService.get(this.login_url+'/user?email='+userName+'&password='+password);
+  }
+  userRegister(userDTO:any):Observable<any>{
+    return this.apiService.post(this.reg_url+'/user',userDTO)
+  }
+  adminLogin(userName: string, password: string): Observable<any>{
+    return this.apiService.get(this.login_url+'/user?email='+userName+'&password='+password+'&role=admin');
+  }
 }
